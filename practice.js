86856a707dd7168766164b809470fe35d3f0ba83
nateHamp <nathanhampton.pt@gmail.com>
2019-01-08 22:51:36 -0700

    completed afternoon 3

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(arr, callback){
+  callback(arr[0]);
+};
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, callback){
+  callback(arr[arr.length-1]);
+  };
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +72,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, callback){
+  callback(num1 * num2);
+};
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +94,13 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr, name, callback){
+  if(arr.includes(name)){
+    return callback(true);
+  } else {
+    return callback(false);
+  }
+};
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +122,15 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(arr, callback){
+  var newArray = [];
+  for(var i=0; i<arr.length; i++){
+    if(!newArray.includes(arr[i])){
+      newArray.push(arr[i])
+    };
+  };
+  return callback(newArray);
+};
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +148,12 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(arr, callback){
+  for(var i=0; i<arr.length; i++){
+    callback(arr[i], i)
+  };
+};
+
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +171,12 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserByID(arr, ident, callback)
+  for(var i=0; i<arr.length; i++){
+    if(arr[i.ident===ident]){
+      callback (arr[i])
+    };
+  };
 
 // Do not edit the code below.
 var users = [
